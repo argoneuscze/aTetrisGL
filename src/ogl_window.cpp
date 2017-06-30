a9ef51b817de38fdbd03fe2304ea744233698d66
argoneus <argoneuscze@gmail.com>
2017-06-30 10:15:10 +0200

    specified minimum OGL version

--- src/ogl_window.cpp
+++ src/ogl_window.cpp
@@ -8,11 +8,19 @@ OGLWindow::OGLWindow(int width, int height, std::string name) {
     if (!glfwInit())
         exit(1);
 
+    glfwWindowHint(GLFW_CONTEXT_VERSION_MAJOR, 3);
+    glfwWindowHint(GLFW_CONTEXT_VERSION_MINOR, 3);
+    glfwWindowHint(GLFW_OPENGL_PROFILE, GLFW_OPENGL_CORE_PROFILE);
+
     this->window = glfwCreateWindow(width, height, name.c_str(), nullptr, nullptr);
+
+    if (!this->window)
+        exit(2);
+
     glfwMakeContextCurrent(this->window);
 
     if (!gladLoadGL())
-        exit(2);
+        exit(3);
 
     printf("OpenGL Version %d.%d loaded", GLVersion.major, GLVersion.minor);
 
